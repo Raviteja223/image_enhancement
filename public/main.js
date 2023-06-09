e143ba9ddeeed77df58799abeb6410c8e8b2c2eb
Raviteja <raviteja.karri@chromiumsolutions.com>
2023-06-09 06:59:14 +0530

    updated

--- public/main.js
+++ public/main.js
@@ -55,3 +55,14 @@ function uploadImage() {
         console.log(error);
     });
 }
+
+function reset() {
+    document.getElementById("inputimg").src = "https://replicate.delivery/mgxm/59d9390c-b415-47e0-a907-f81b0d9920f1/187400315-87a90ac9-d231-45d6-b377-38702bd1838f.jpg";
+    document.getElementById("outputimg").src = "https://replicate.delivery/mgxm/59d9390c-b415-47e0-a907-f81b0d9920f1/187400315-87a90ac9-d231-45d6-b377-38702bd1838f.jpg";
+    document.getElementById("file").value = "";
+    base64Image = "";
+}
+
+function imageopen(url) {
+    window.open(url);
+}
